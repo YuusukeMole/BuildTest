8ce42b931d695d679ceb085c18827bc6a9df1a31
LAPTOP-TAFL9MMM\syuu1 <s.yuu12.nawate@gmail.com>
2017-03-03 21:53:35 +0900

    ビルドのテスト

---
+++ BuildTest/Assets/C#/ChangeTextureEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTextureEnemy : MonoBehaviour {
+	public Material OriginalMaterial;
+	public Material DamageMaterial;
+	private Renderer gameobjectrenderer;
+
+	void Start()
+	{
+		gameobjectrenderer = GetComponent<Renderer> ();
+	}
+		
+	void OnCollisionEnter(Collision collision)
+	{
+		var other = collision.gameObject;
+		if (other.layer == 11) {
+			gameobjectrenderer.material.color = DamageMaterial.color;
+		} else {
+			gameobjectrenderer.material.color = OriginalMaterial.color;
+		}
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		gameobjectrenderer.material.color = OriginalMaterial.color;
+	}
+
+
+}
